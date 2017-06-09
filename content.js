6808d00a4b13c0b05b7bc81d4a9abb20e2ac5815
Dmytro Petruk <bavaria95@gmail.com>
2017-06-09 21:25:42 +0200

    finding posts of needed account

---
+++ content.js
@@ -0,0 +1,7 @@
+function hide(account) {
+    var posts = document.getElementsByTagName("article");
+
+    for (var post of posts)
+        if (post.firstChild.children[1].children[0].textContent == account)
+            console.log(post)
+}
