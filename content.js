f6cecc662d4b957d4cd29ec7a181e62d40e99989
Dmytro Petruk <bavaria95@gmail.com>
2017-06-09 21:27:58 +0200

    executing hiding function

--- content.js
+++ content.js
@@ -1,7 +1,9 @@
 function hide(account) {
-    var posts = document.getElementsByTagName("article");
+    var posts = document.getElementsByTagName("article")
 
     for (var post of posts)
         if (post.firstChild.children[1].children[0].textContent == account)
-            post.style = "display:none;";
+            post.style = "display:none;"
 }
+
+window.onload = function(){hide('justinbieber')}
